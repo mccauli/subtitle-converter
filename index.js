e76cf86bed97e4475e8f888bbaa7eb571643a435
Derek <derek@adrise.com>
2018-12-19 17:27:40 -0800

    plug in convert function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const parse = require('./lib/parsers');
-const validate = require('./lib/validators');
+// const validate = require('./lib/validators');
 const convert = require('./lib/converters');
 
 module.exports = {
-  parse,
-  validate,
+  // validate,
   convert,
 };
