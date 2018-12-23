059ed611db5d8850ee788fe37fd8f31989c1848a
Ian McCaul <iancmccaul@gmail.com>
2018-12-23 14:38:26 -0800

    integrate transformers, finish srt implementation

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// const validate = require('./lib/validators');
 const convert = require('./lib/converters');
 
+convert(
+  '/Users/ian/REPOSITORIES/janus/tests/test_subs/good_srt.srt',
+  '/tmp/test_srt.srt',
+  {
+    removeTextFormatting: true,
+    shiftTimecode: 3,
+    sourceFps: 25,
+    outputFps: 23.976,
+  },
+)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+
 module.exports = {
-  // validate,
   convert,
 };
